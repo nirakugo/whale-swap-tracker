61f730a1a523fbc16c4730d925a9252a25f43659
nirakugo <163942528+nirakugo@users.noreply.github.com>
2025-06-27 05:08:18 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
